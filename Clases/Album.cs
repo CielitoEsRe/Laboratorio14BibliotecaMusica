c93a0dac3bbd4724d0cc5099b3ecee07b6c065ff
Cielo Estrada <cielo_estrada07@umes.edu.gt>
2026-05-21 21:48:16 -0600

    Agregar archivos de proyecto.

---
+++ Clases/Album.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio14BibliotecaMusica.Clases
+{
+    public class Album
+    {
+        public string Titulo { get; set; }
+
+        public string ArtistaAlbum { get; set; }
+
+        public DateTime FechaPublicacion { get; set; }
+
+        public List<Cancion> Canciones { get; set; }
+
+        public Album()
+        {
+            Canciones = new List<Cancion>();
+        }
+    }
+}
---
+++ Clases/Cancion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio14BibliotecaMusica.Clases
+{
+    public class Cancion
+    {
+        public string Nombre { get; set; }
+
+        public string Artista { get; set; }
+
+        public string Duracion { get; set; }
+    }
+}
