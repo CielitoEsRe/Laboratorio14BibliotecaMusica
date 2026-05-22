6d813c8a7699bb0dec4e80f299b2766bc6b94317
Cielo Estrada <cielo_estrada07@umes.edu.gt>
2026-05-21 22:07:30 -0600

    Creacion del formulario

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,234 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtArtista = new System.Windows.Forms.TextBox();
+            this.txtCancion = new System.Windows.Forms.TextBox();
+            this.txtDuracion = new System.Windows.Forms.TextBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.dataGridViewAlbumes = new System.Windows.Forms.DataGridView();
+            this.dateTimePickerFecha = new System.Windows.Forms.DateTimePicker();
+            this.listBoxCanciones = new System.Windows.Forms.ListBox();
+            this.buttonAgregarCancion = new System.Windows.Forms.Button();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAlbumes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(112, 48);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(208, 22);
+            this.txtTitulo.TabIndex = 0;
+            // 
+            // txtArtista
+            // 
+            this.txtArtista.Location = new System.Drawing.Point(112, 118);
+            this.txtArtista.Name = "txtArtista";
+            this.txtArtista.Size = new System.Drawing.Size(208, 22);
+            this.txtArtista.TabIndex = 1;
+            // 
+            // txtCancion
+            // 
+            this.txtCancion.Location = new System.Drawing.Point(141, 193);
+            this.txtCancion.Name = "txtCancion";
+            this.txtCancion.Size = new System.Drawing.Size(208, 22);
+            this.txtCancion.TabIndex = 2;
+            // 
+            // txtDuracion
+            // 
+            this.txtDuracion.Location = new System.Drawing.Point(112, 287);
+            this.txtDuracion.Name = "txtDuracion";
+            this.txtDuracion.Size = new System.Drawing.Size(208, 22);
+            this.txtDuracion.TabIndex = 3;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(112, 362);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(208, 22);
+            this.txtBuscar.TabIndex = 4;
+            // 
+            // dataGridViewAlbumes
+            // 
+            this.dataGridViewAlbumes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAlbumes.Location = new System.Drawing.Point(817, 48);
+            this.dataGridViewAlbumes.Name = "dataGridViewAlbumes";
+            this.dataGridViewAlbumes.RowHeadersWidth = 51;
+            this.dataGridViewAlbumes.RowTemplate.Height = 24;
+            this.dataGridViewAlbumes.Size = new System.Drawing.Size(332, 328);
+            this.dataGridViewAlbumes.TabIndex = 5;
+            // 
+            // dateTimePickerFecha
+            // 
+            this.dateTimePickerFecha.Location = new System.Drawing.Point(445, 48);
+            this.dateTimePickerFecha.Name = "dateTimePickerFecha";
+            this.dateTimePickerFecha.Size = new System.Drawing.Size(268, 22);
+            this.dateTimePickerFecha.TabIndex = 6;
+            // 
+            // listBoxCanciones
+            // 
+            this.listBoxCanciones.FormattingEnabled = true;
+            this.listBoxCanciones.ItemHeight = 16;
+            this.listBoxCanciones.Location = new System.Drawing.Point(445, 108);
+            this.listBoxCanciones.Name = "listBoxCanciones";
+            this.listBoxCanciones.Size = new System.Drawing.Size(286, 196);
+            this.listBoxCanciones.TabIndex = 7;
+            // 
+            // buttonAgregarCancion
+            // 
+            this.buttonAgregarCancion.Location = new System.Drawing.Point(461, 373);
+            this.buttonAgregarCancion.Name = "buttonAgregarCancion";
+            this.buttonAgregarCancion.Size = new System.Drawing.Size(102, 41);
+            this.buttonAgregarCancion.TabIndex = 8;
+            this.buttonAgregarCancion.Text = "Agregar Cancion";
+            this.buttonAgregarCancion.UseVisualStyleBackColor = true;
+            this.buttonAgregarCancion.Click += new System.EventHandler(this.buttonAgregarCancion_Click);
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(624, 373);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(107, 41);
+            this.buttonGuardar.TabIndex = 9;
+            this.buttonGuardar.Text = "Guardar Album";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(461, 471);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(102, 40);
+            this.buttonEditar.TabIndex = 10;
+            this.buttonEditar.Text = "Editar Album";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            this.buttonEditar.Click += new System.EventHandler(this.buttonEditar_Click);
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(624, 471);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(107, 40);
+            this.buttonBuscar.TabIndex = 11;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Titulo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 123);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Artista";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Nombre Cancion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Duracion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 367);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Buscar rtista";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(445, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Fecha";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1178, 556);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.buttonAgregarCancion);
+            this.Controls.Add(this.listBoxCanciones);
+            this.Controls.Add(this.dateTimePickerFecha);
+            this.Controls.Add(this.dataGridViewAlbumes);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.txtDuracion);
+            this.Controls.Add(this.txtCancion);
+            this.Controls.Add(this.txtArtista);
+            this.Controls.Add(this.txtTitulo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAlbumes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.TextBox txtArtista;
+        private System.Windows.Forms.TextBox txtCancion;
+        private System.Windows.Forms.TextBox txtDuracion;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.DataGridView dataGridViewAlbumes;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFecha;
+        private System.Windows.Forms.ListBox listBoxCanciones;
+        private System.Windows.Forms.Button buttonAgregarCancion;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Button buttonEditar;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,159 @@
-﻿using System;
+﻿using Laboratorio14BibliotecaMusica.Clases;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Laboratorio14BibliotecaMusica
 {
     public partial class Form1 : Form
     {
+        List<Album> listaAlbumes = new List<Album>();
+
+        string archivo = "albumes.json";
+
+        List<Cancion> cancionesTemp = new List<Cancion>();
+
+        int indiceEditar = -1;
+
         public Form1()
         {
             InitializeComponent();
+
+            CargarDatos(); ;
         }
+
+        private void buttonAgregarCancion_Click(object sender, EventArgs e)
+        {
+            Cancion nueva = new Cancion();
+
+            nueva.Nombre = txtCancion.Text;
+            nueva.Artista = txtArtista.Text;
+            nueva.Duracion = txtDuracion.Text;
+
+            cancionesTemp.Add(nueva);
+
+            listBoxCanciones.Items.Add(nueva.Nombre);
+
+            txtCancion.Clear();
+            txtDuracion.Clear();
+        }
+
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+            Album nuevo = new Album();
+
+            nuevo.Titulo = txtTitulo.Text;
+            nuevo.ArtistaAlbum = txtArtista.Text;
+            nuevo.FechaPublicacion = dateTimePickerFecha.Value;
+            nuevo.Canciones = cancionesTemp;
+
+            listaAlbumes.Add(nuevo);
+
+            GuardarDatos();
+
+            MostrarDatos();
+
+            Limpiar();
+        }
+
+        private void MostrarDatos()
+        {
+            dataGridViewAlbumes.DataSource = null;
+
+            dataGridViewAlbumes.DataSource = listaAlbumes.Select(a => new
+            {
+                a.Titulo,
+                a.ArtistaAlbum,
+                Fecha = a.FechaPublicacion.ToShortDateString(),
+                CantidadCanciones = a.Canciones.Count
+            }).ToList();
+        }
+
+        private void GuardarDatos()
+        {
+            string json = JsonConvert.SerializeObject(listaAlbumes, Newtonsoft.Json.Formatting.Indented);
+
+            File.WriteAllText(archivo, json);
+        }
+
+        private void CargarDatos()
+        {
+            if (File.Exists(archivo))
+            {
+                string json = File.ReadAllText(archivo);
+
+                listaAlbumes = JsonConvert.DeserializeObject<List<Album>>(json);
+
+                MostrarDatos();
+            }
+        }
+
+        private void Limpiar()
+        {
+            txtTitulo.Clear();
+            txtArtista.Clear();
+            txtCancion.Clear();
+            txtDuracion.Clear();
+
+            cancionesTemp = new List<Cancion>();
+
+            listBoxCanciones.Items.Clear();
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            string artista = txtBuscar.Text.ToLower();
+
+            dataGridViewAlbumes.DataSource = null;
+
+            dataGridViewAlbumes.DataSource = listaAlbumes
+                .Where(a => a.ArtistaAlbum.ToLower().Contains(artista))
+                .Select(a => new
+                {
+                    a.Titulo,
+                    a.ArtistaAlbum,
+                    Fecha = a.FechaPublicacion.ToShortDateString(),
+                    CantidadCanciones = a.Canciones.Count
+                }).ToList();
+        }
+
+        private void buttonEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewAlbumes.CurrentRow != null)
+            {
+                indiceEditar = dataGridViewAlbumes.CurrentRow.Index;
+
+                txtTitulo.Text = listaAlbumes[indiceEditar].Titulo;
+
+                txtArtista.Text = listaAlbumes[indiceEditar].ArtistaAlbum;
+
+                dateTimePickerFecha.Value = listaAlbumes[indiceEditar].FechaPublicacion;
+
+                cancionesTemp = listaAlbumes[indiceEditar].Canciones;
+
+                listBoxCanciones.Items.Clear();
+
+                foreach (var c in cancionesTemp)
+                {
+                    listBoxCanciones.Items.Add(c.Nombre);
+                }
+
+                listaAlbumes.RemoveAt(indiceEditar);
+
+                GuardarDatos();
+
+                MostrarDatos();
+            }
+    }
+
     }
 }
